705f593bd699a41d2d03ba84566840ebddc60c06
Kevin Sutton <kevmsutton@gmail.com>
2018-11-12 18:00:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(elements)
 first_elements = elements.join (", ")
 	if elements.length == 1
-	return #{elements[1]}
+	return elements.pop
   elsif elements.length == 2
   return "#{elements[0]}and #{elements[1]}"
 end
