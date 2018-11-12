4bf43eb621f15735c016f645e8babcf4c83f09d8
Kevin Sutton <kevmsutton@gmail.com>
2018-11-12 17:58:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,11 +4,6 @@ first_elements = elements.join (", ")
 	return #{elements[1]}
   elsif elements.length == 2
   return "#{elements[0]}and #{elements[1]}"
-  return 
-last = elements.pop
-	elsif elements.length >2
-	return "#{first_elements}, and #{last}"
-end
-end
+
 
 oxford_comma(["fiddleheads"])
