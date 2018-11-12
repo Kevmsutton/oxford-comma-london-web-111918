a0747d7618ceca56130e8f34b0649a854262c690
Kevin Sutton <kevmsutton@gmail.com>
2018-11-12 17:38:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(elements)
 	last = elements[-1]
-	new_array = []
 	first_elements = elements.join (", ")
-	return "#{first_elements} and #{last}"
+	return "#{elements} and #{last}"
 end
 
 oxford_comma(["fiddleheads","okra","kohlrabi"])
